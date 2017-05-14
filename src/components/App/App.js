d983d4313ea1be1193a10ba9dc7221476d4fe011
Vicent Gozalbes <vigosan@gmail.com>
2017-05-14 21:25:32 +0200

    Extract Message component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Message from '../Message'
 import styles from './App.css'
 
 const App = ({ message }) => (
   <div className={styles.container}>
-    <span className={styles.message}>{message}</span>
+    <Message text="It Works!" />
   </div>
 )
 
